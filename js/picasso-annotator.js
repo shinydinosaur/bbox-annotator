55d5aaf3448457da38158cbb3fd1a3def9dd61b2
Shiry Ginosar <shiry@shiny.local>
2013-10-28 15:59:15 -0700

    adding some timing information and a variable to hold the bucket answer

--- js/picasso-annotator.js
+++ js/picasso-annotator.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  // grab some timing info
+  var t0 = window.performance.now();
+
   // Initialize the bounding-box annotator.
   var annotator = new BBoxAnnotator({
     url: "/Users/shiry/Documents/Projects/Art_Vision/Picasso/People/A-Young-Faun-Playing-a-Serenade-to-a-Young-Girl-1938.JPG",
@@ -11,8 +14,21 @@ $(document).ready(function() {
       $("#annotation_data").text(JSON.stringify(entries, null, "  "));
     }
   });
+  
   // Initialize the reset button.
   $("#reset_button").click(function(e) {
     annotator.clear_all();
   })
+
+  // When a button is clicked in the is this human? buckets question.
+  $("#buckets").button(function() {
+  	$("#bucket").val($(this).text());
+  });
+
+  $("#submit").click(function() { 
+  	// how are we passing data?
+  	// entries, bucket, time
+	var t1 = window.performance.now();
+	var time = t1-t0; // in milliseconds
+  }
 });
