3b9ff1a28accc986bb668fd9662ea76e1a835c4c
Daniel Haas <dhaas@eecs.berkeley.edu>
2014-01-16 23:25:16 -0800

    Added false positives and false negatives

--- cluster_annotations.py
+++ cluster_annotations.py
@@ -130,7 +130,7 @@ def plot_annotations(imgid, clustered_annotations):
     return True
 
 def annotations_by_user(annotations, userids=None):
-    by_user = {}
+    by_user = { userid: [] for userid in userids } if userids else {}
     for annotation, cluster_id in annotations:
         if userids is None or annotation.userid in userids:
             if annotation.userid not in by_user:
@@ -145,54 +145,91 @@ def annotations_by_cluster(annotations, clusterids=None):
             if cluster_id not in by_cluster:
                 by_cluster[cluster_id] = []
             by_cluster[cluster_id].append((annotation, cluster_id))
-    return by_cluster
-        
+    return by_cluster        
 
 def overlap_error_score(user1_annotation, user2_annotation):
     # compute as 1 - box intersection / box union (this is bi-directional)
     return 1.0 - (user1_annotation.intersection_size(user2_annotation) / user1_annotation.union_size(user2_annotation))
 
 if __name__ == '__main__':
-    user_annotations = parse_annotations(ANNOTATION_PATH)
-    out = {}
+    annotations = parse_annotations(ANNOTATION_PATH)
     scores = {}
-    for imgid, annotation_list in user_annotations.iteritems():
+    fps = {}
+    fns = {}
+    for imgid, annotation_list in annotations.iteritems():
         
         # cluster the annotations
         clustered_annotations = cluster_annotations(annotation_list)
-        out[imgid] = clustered_annotations
         if random.random() < PCT_IMAGES_TO_PLOT / 100.0:
             plotted = plot_annotations(imgid, clustered_annotations)        
         
         # compute pairwise error scores for each figure
+        by_cluster = annotations_by_cluster(clustered_annotations)
+        all_users = annotations_by_user(clustered_annotations).keys()
         overlap_scores = {}
-        for clusterid, annotation_list in annotations_by_cluster(clustered_annotations).iteritems():
+        false_positives = {}
+        false_negatives = {}
+        for user1, user2 in combinations(all_users, 2):
+            dict_key = str(sorted((user1, user2))) # allow bidirectional lookup
+            u1_key = str(user1)
+            u2_key = str(user2)
+            overlap_scores[dict_key] = []
+            false_positives[u1_key] = 0
+            false_positives[u2_key] = 0
+            false_negatives[u1_key] = []
+            false_negatives[u2_key] = []
 
-            # ignore the outlier cluster
-            if clusterid == -1:
-                continue
+            for cluster_id, annotation_list in by_cluster.iteritems():
+                u1_false_positives = u2_false_positives = 0
+                u1_false_negatives = u2_false_negatives = 0
+                user_annotations = annotations_by_user(annotation_list, [user1, user2])
 
-            by_user = annotations_by_user(annotation_list)
-            userids = by_user.keys()
-            for user1, user2 in combinations(userids, 2):
-                user1_annotations = by_user[user1]
-                user2_annotations = by_user[user2]
-                if len(user1_annotations) != 1:
-                    # print "WARNING: user %d had multiple annotations of person %d in image %s" % (user1, clusterid, imgid)
-                    user1_annotations[0] = random.choice(user1_annotations)
+                # Handle the outlier cluster--these are all false positives.
+                if cluster_id == -1: 
+                    u1_false_positives += len(user_annotations[user1])
+                    u2_false_positives += len(user_annotations[user2])
+                    false_positives[u1_key] = u1_false_positives
+                    false_positives[u2_key] = u2_false_positives
+                    continue
 
-                if len(user2_annotations) != 1:
-                    # print "WARNING: user %d had multiple annotations of person %d in image %s" % (user2, clusterid, imgid)
-                    user2_annotations[0] = random.choice(user2_annotations)
+                # Handle False negatives: users didn't draw a box for this figure.
+                overlap_possible = True
+                if len(user_annotations[user1]) == 0:
+                    u1_false_negatives += 1 
+                    overlap_possible = False
+                if len(user_annotations[user2]) == 0:
+                    u2_false_negatives += 1
+                    overlap_possible = False
+                false_negatives[u1_key].append(u1_false_negatives)
+                false_negatives[u2_key].append(u2_false_negatives)
+                if not overlap_possible:
+                    continue
 
-                overlap_score = overlap_error_score(user1_annotations[0][0], user2_annotations[0][0])
-                dict_key = str(sorted((user1, user2))) # allow bidirectional lookup
-                if dict_key not in overlap_scores:
-                    overlap_scores[dict_key] = []
-                overlap_scores[dict_key].append(overlap_score)
+                # Otherwise, overlap scores
+                if len(user_annotations[user1]) != 1:
+                    # Either user accidentally drew too many rectangles or algorithm
+                    # misclassified a rectangle because it was too close to the cluster.
+                    # Either way, pick a random rectangle to evaluate.
+                    user_annotations[user1][0] = random.choice(user_annotations[user1])
+                if len(user_annotations[user2]) != 1:
+                    user_annotations[user2][0] = random.choice(user_annotations[user2])
+                overlap_scores[dict_key].append(
+                    overlap_error_score(
+                        user_annotations[user1][0][0], 
+                        user_annotations[user2][0][0]))
+
+            # Aggregate False Positives and False Negatives over the image.
+            # False Positives are fine as is.
+            # Score False negatives as #FN / #Clusters.
+            u1_fn_scores = false_negatives[u1_key]
+            u2_fn_scores = false_negatives[u2_key]            
+            false_negatives[u1_key] = np.mean(u1_fn_scores) if u1_fn_scores else 0.0
+            false_negatives[u2_key] = np.mean(u2_fn_scores) if u2_fn_scores else 0.0
 
         # Average overlap scores per user pair per image
-        scores[imgid] = { key : np.mean(overlap_score_list) for key, overlap_score_list in overlap_scores.iteritems() }
+        scores[imgid] = { key : np.mean(overlap_score_list) for key, overlap_score_list in overlap_scores.iteritems() if overlap_score_list }
+        fps[imgid] = false_positives
+        fns[imgid] = false_negatives
 
     # plot the distribution of error between pairs of users on the same image
     plt.figure()
@@ -203,15 +240,50 @@ if __name__ == '__main__':
     plt.title("Overlap Error scores (same images)")
     plt.show()
 
+    # Draw a boxplot by image
+    # use ~1/10 of the images so plot is readable
+    boxplot_data = [img_dict.values() for img_dict in scores.values()]
+    num_images = len(boxplot_data)
+    sample_indexes = sorted(random.sample(range(num_images), num_images / 10))
+    boxplot_data = [img_scores for i, img_scores in enumerate(boxplot_data) if i in sample_indexes]
+    img_labels = ["Image %s" % str(i) for i in sample_indexes]
+    fig = plt.figure()
+    ax = plt.gca()
+    plt.boxplot(boxplot_data, notch=False, sym='')
+    xtickNames = plt.setp(ax, xticklabels=img_labels)
+    plt.setp(xtickNames, rotation=45, fontsize=8)
+    plt.show()
+
+    # plot false positives
+    plt.figure()
+    data_fp = [img_dict.values() for i, img_dict in enumerate(fps.values()) if i in sample_indexes]
+    ax = plt.gca()
+    plt.boxplot(data_fp, sym='')
+    xtickNames = plt.setp(ax, xticklabels=img_labels)
+    plt.setp(xtickNames, rotation=45, fontsize=8)
+    plt.title("False Positive Scores")
+    plt.ylim([-1, max([max(scores) for scores in data_fp]) + 2])
+    plt.show()
+
+    # plot false negatives
+    plt.figure()
+    data_fn = [img_dict.values() for i, img_dict in enumerate(fns.values()) if i in sample_indexes]
+    ax = plt.gca()
+    plt.boxplot(data_fn, sym='')
+    xtickNames = plt.setp(ax, xticklabels=img_labels)
+    plt.setp(xtickNames, rotation=45, fontsize=8)
+    plt.title("False Negative Scores")
+    plt.ylim([-1, 2])
+    plt.show()
+
+    exit() # Don't wait forever for the last bit!
+
     # plot the distribution of error scores between pairs of users on different images
-    all_annotations = [user_annotation for user_annotation_list in user_annotations.itervalues() for user_annotation in user_annotation_list]
+    all_annotations = [annotation for user_annotation_list in annotations.itervalues() for annotation in user_annotation_list]
     data_different = []
     for anno1, anno2 in combinations(all_annotations, 2):
         if anno1.imgid != anno2.imgid:
-            try:
-                data_different.append(overlap_error_score(anno1, anno2))
-            except ZeroDivisionError:
-                import pdb;pdb.set_trace()
+            data_different.append(overlap_error_score(anno1, anno2))
     plt.figure()
     n, bins, patches = plt.hist(data_different, 20, histtype='bar', normed=True)
     plt.setp(patches, 'facecolor', 'b', 'alpha', 0.75)
